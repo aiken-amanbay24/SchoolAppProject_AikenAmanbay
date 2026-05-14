3767661e637767fa3800a115589fb1eb2578f49c
Aiken <aiespa.234@gmail.com>
2026-05-14 20:03:50 +0500

    Initial Commit

---
+++ SchoolApp/SchoolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolAppApp.swift
+//  SchoolApp
+//
+//  Created by Aiken on 14.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct SchoolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
