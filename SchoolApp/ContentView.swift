9e0dc15116c51077364b6ed780cff59321268749
Aiken <aiespa.234@gmail.com>
2026-05-14 20:17:40 +0500

    Initial commit

--- SchoolApp/ContentView.swift
+++ SchoolApp/ContentView.swift
@@ -1,19 +1,33 @@
-//
-//  ContentView.swift
-//  SchoolApp
-//
-//  Created by Aiken on 14.05.2026.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var count = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 25) {
+
+            Text("School App")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            Text("Powered by SwiftUI")
+                .font(.title3)
+
+            Text("Checked \(count) times")
+                .font(.title2)
+
+            Button(action: {
+                count += 1
+            }) {
+
+                Text("Check")
+                    .font(.title3)
+                    .foregroundColor(.white)
+                    .frame(width: 200, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(12)
+            }
         }
         .padding()
     }
